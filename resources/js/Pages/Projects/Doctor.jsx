999cf9f7778cbe70b2839fc30c67963680d205aa
Frank Olayinka Favour <favescsskr@gmail.com>
2023-08-01 19:35:33 +0100

    doctor project

--- resources/js/Pages/Projects/Doctor.jsx
+++ resources/js/Pages/Projects/Doctor.jsx
@@ -1,9 +1,14 @@
+import { Head } from "@inertiajs/react";
 import React from "react";
 import ScrollAnimation from "react-animate-on-scroll";
 
 function Doctor() {
     return (
         <div className="font-poppins">
+            <Head>
+                <title>Frankly Dev- Project Doctor</title>
+                <meta name="description" content="Redesign of doctor UI into a web page using ReactJs and TailwindCSS" />
+            </Head>
             <div className="w-full bg-[#082144] text-white p-5 px-16 pb-28 relative">
                 <div className="flex items-center justify-between">
                     <p className="font-bold">Delta.io</p>
@@ -72,11 +77,11 @@ function Doctor() {
                     </ScrollAnimation>
                 </div>
                 <div className="absolute w-2/3 mx-auto flex justify-center -bottom-52 z-50 left-0 right-0">
-                <ScrollAnimation animateIn="fadeInUp">
-                    <img
-                        src="/images/doctor.jpeg"
-                        className="w-[32em] h-72 object-cover rounded-2xl"
-                    />
+                    <ScrollAnimation animateIn="fadeInUp">
+                        <img
+                            src="/images/doctor.jpeg"
+                            className="w-[32em] h-72 object-cover rounded-2xl"
+                        />
                     </ScrollAnimation>
                 </div>
             </div>
@@ -170,158 +175,159 @@ function Doctor() {
                 </div>
             </ScrollAnimation>
             <ScrollAnimation animateIn="fadeInUp">
-            <div className="flex items-center space-x-24 px-24 py-16">
-                <div className="space-y-8">
-                    <div className="">
-                        <p className="font-bold text-[#082144]">
-                            Know about our special tips and tricks
-                        </p>
-                        <p className="text-xs text-gray-400 text-light">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Aperiam dolorum rem expedita itaque et nihil.
-                        </p>
-                    </div>
-
+                <div className="flex items-center space-x-24 px-24 py-16">
                     <div className="space-y-8">
-                        <div className="flex items-center space-x-4">
-                            <span className="h-12 w-12 rounded-xl bg-[#082144]"></span>
-                            <div className="">
-                                <p className="text-[#082144] font-bold">
-                                    How does it work
-                                </p>
-                                <p className="text-xs text-gray-600">
-                                    Lorem ipsum, dolor sit amet consectetur
-                                    adipisicing elit.
-                                </p>
-                            </div>
+                        <div className="">
+                            <p className="font-bold text-[#082144]">
+                                Know about our special tips and tricks
+                            </p>
+                            <p className="text-xs text-gray-400 text-light">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Aperiam dolorum rem expedita
+                                itaque et nihil.
+                            </p>
                         </div>
-                        <div className="flex items-center space-x-4">
-                            <span className="h-12 w-12 rounded-xl bg-[#082144]">
-                                123
-                            </span>
-                            <div className="">
-                                <p className="text-[#082144] font-bold">
-                                    Option 2 goes here
-                                </p>
-                                <p className="text-xs text-gray-600">
-                                    Lorem ipsum, dolor sit amet consectetur
-                                    adipisicing elit.
-                                </p>
+
+                        <div className="space-y-8">
+                            <div className="flex items-center space-x-4">
+                                <span className="h-12 w-12 rounded-xl bg-[#082144]"></span>
+                                <div className="">
+                                    <p className="text-[#082144] font-bold">
+                                        How does it work
+                                    </p>
+                                    <p className="text-xs text-gray-600">
+                                        Lorem ipsum, dolor sit amet consectetur
+                                        adipisicing elit.
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex items-center space-x-4">
-                            <span className="h-12 w-12 rounded-xl bg-[#082144]"></span>
-                            <div className="">
-                                <p className="text-[#082144] font-bold">
-                                    Third option here
-                                </p>
-                                <p className="text-xs text-gray-600">
-                                    Lorem ipsum, dolor sit amet consectetur
-                                    adipisicing elit.
-                                </p>
+                            <div className="flex items-center space-x-4">
+                                <span className="h-12 w-12 rounded-xl bg-[#082144]">
+                                    123
+                                </span>
+                                <div className="">
+                                    <p className="text-[#082144] font-bold">
+                                        Option 2 goes here
+                                    </p>
+                                    <p className="text-xs text-gray-600">
+                                        Lorem ipsum, dolor sit amet consectetur
+                                        adipisicing elit.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="flex items-center space-x-4">
+                                <span className="h-12 w-12 rounded-xl bg-[#082144]"></span>
+                                <div className="">
+                                    <p className="text-[#082144] font-bold">
+                                        Third option here
+                                    </p>
+                                    <p className="text-xs text-gray-600">
+                                        Lorem ipsum, dolor sit amet consectetur
+                                        adipisicing elit.
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="relative w-5/12 flex">
+                        <img
+                            src="/images/nurse-2.png"
+                            alt="hhh"
+                            className="block w-2/3"
+                        />
+                    </div>
                 </div>
-                <div className="relative w-5/12 flex">
-                    <img
-                        src="/images/nurse-2.png"
-                        alt="hhh"
-                        className="block w-2/3"
-                    />
-                </div>
-            </div>
             </ScrollAnimation>
 
             <ScrollAnimation animateIn="fadeInUp">
-            <div className="flex space-x-24 px-32 items-center">
-                <div className="w-1/2 p-8">
-                    <img src="/images/nurse-3.png" className="w-" />
-                </div>
-                <div className="w-1/2 space-y-8">
-                    <div className="space-y-1">
-                        <p className="font-bold text-[#082144]">
-                            Frequently asked questions
-                        </p>
-                        <p className="text-xs text-light text-gray-600">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing
-                            elit. Ipsam cupiditate architecto, numquam
-                            praesentium ea non dignissimos quas
-                        </p>
+                <div className="flex space-x-24 px-32 items-center">
+                    <div className="w-1/2 p-8">
+                        <img src="/images/nurse-3.png" className="w-" />
                     </div>
+                    <div className="w-1/2 space-y-8">
+                        <div className="space-y-1">
+                            <p className="font-bold text-[#082144]">
+                                Frequently asked questions
+                            </p>
+                            <p className="text-xs text-light text-gray-600">
+                                Lorem ipsum dolor, sit amet consectetur
+                                adipisicing elit. Ipsam cupiditate architecto,
+                                numquam praesentium ea non dignissimos quas
+                            </p>
+                        </div>
 
-                    <div>
-                        <p className="font-bold text-[#082144] text-xs">
-                            Some random text here
-                        </p>
-                        <p className="text-xs font-light text-gray-600">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Tempora similique explicabo, molestias maiores
-                            cumque, laudantium excepturi
-                        </p>
-                    </div>
+                        <div>
+                            <p className="font-bold text-[#082144] text-xs">
+                                Some random text here
+                            </p>
+                            <p className="text-xs font-light text-gray-600">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Tempora similique explicabo,
+                                molestias maiores cumque, laudantium excepturi
+                            </p>
+                        </div>
 
-                    <div className="space-y-4">
-                        <div className="flex justify-between items-center text-[#082144]">
-                            <p className="font-bold text-[#082144] text-xs">
-                                Question 1 is a question
-                            </p>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="w-4 h-4"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                                />
-                            </svg>
-                        </div>
-                        <div className="flex justify-between items-center text-[#082144]">
-                            <p className="font-bold text-[#082144] text-xs">
-                                How much for a demo session?
-                            </p>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="w-4 h-4"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                                />
-                            </svg>
-                        </div>
-                        <div className="flex justify-between items-center text-[#082144]">
-                            <p className="font-bold text-[#082144] text-xs">
-                                What about when there is no doctor?
-                            </p>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="w-4 h-4"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-                                />
-                            </svg>
+                        <div className="space-y-4">
+                            <div className="flex justify-between items-center text-[#082144]">
+                                <p className="font-bold text-[#082144] text-xs">
+                                    Question 1 is a question
+                                </p>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    className="w-4 h-4"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="flex justify-between items-center text-[#082144]">
+                                <p className="font-bold text-[#082144] text-xs">
+                                    How much for a demo session?
+                                </p>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    className="w-4 h-4"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="flex justify-between items-center text-[#082144]">
+                                <p className="font-bold text-[#082144] text-xs">
+                                    What about when there is no doctor?
+                                </p>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke-width="1.5"
+                                    stroke="currentColor"
+                                    className="w-4 h-4"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                                    />
+                                </svg>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </ScrollAnimation>
             <div className="grid grid-cols-4 gap-8 bg-[#082144] text-white p-8 px-24">
                 <div className="space-y-8">
